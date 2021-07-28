18088d456b173c731376a89f3acb271c612ab930
Madhavi Pothuganti <MPothuganti@thinkingcapital.ca>
2021-07-28 19:40:05 -0400

    added new SOS statement

--- src/Test.java
+++ src/Test.java
@@ -3,6 +3,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Sample");
+		System.out.println("Created new branch Ticket1 to push the code to the branch===>");
 	}
 
 }
